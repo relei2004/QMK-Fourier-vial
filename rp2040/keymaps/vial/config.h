54ff463b1ca1062422823ea75e0ecc31515c5c39
relei2004 <98670229+relei2004@users.noreply.github.com>
2026-05-18 13:33:29 +0200

    Update config.h

--- rp2040/keymaps/vial/config.h
+++ rp2040/keymaps/vial/config.h
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 #pragma once
-
+#define FORCE_NKRO_OFF
 #define VIAL_KEYBOARD_UID {0x2F, 0x4C, 0x8A, 0x1D, 0x6E, 0xB3, 0x95, 0x71}
 #define VIAL_UNLOCK_COMBO_ROWS {0, 1}
 #define VIAL_UNLOCK_COMBO_COLS {0, 1}
